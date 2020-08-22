5814e4c2177bbe12af3d2e51cd61792a4cb10721
PratikGarai <pratikgarai0208@gmail.com>
2020-08-23 01:56:38 +0530

    Added the buttons

--- MainMenu.py
+++ MainMenu.py
@@ -1,5 +1,20 @@
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 
 class MainMenu(FloatLayout):
-    pass
+    def __init__(self, **kwargs):
+        super(MainMenu, self).__init__(**kwargs)
+        self.add_widget(Label(text='Clicker Game'))
+        self.add_widget(StartButton())
+        self.add_widget(QuitButton())
+
+class StartButton(Button):
+    def __init__(self,**kwargs):
+        super(StartButton,self).__init__(**kwargs)
+        self.text = 'Start Game'
+
+class QuitButton(Button):
+    def __init__(self,**kwargs):
+        super(QuitButton,self).__init__(**kwargs)
+        self.text = 'Quit Game'
