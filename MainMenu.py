d8c7fdb7e1f256cf9a5fb3bbb84e51f386dcd62a
PratikGarai <pratikgarai0208@gmail.com>
2020-08-23 02:16:08 +0530

    Made the home screen structure

--- MainMenu.py
+++ MainMenu.py
@@ -5,16 +5,30 @@ from kivy.uix.label import Label
 class MainMenu(FloatLayout):
     def __init__(self, **kwargs):
         super(MainMenu, self).__init__(**kwargs)
-        self.add_widget(Label(text='Clicker Game'))
+        self.add_widget(HeadLabel())
         self.add_widget(StartButton())
         self.add_widget(QuitButton())
+
+class HeadLabel(Label):
+    def __init__(self,**kwargs):
+        super(HeadLabel,self).__init__(**kwargs)
+        self.pos_hint = { 'top':0.85, 'center_x':0.5 }
+        self.size_hint = (0.8,0.25)
+        self.text = '[b][size=50]Clicker Game[/size][/b][sub]Pratik Garai[/sub]'
+        self.markup = True
 
 class StartButton(Button):
     def __init__(self,**kwargs):
         super(StartButton,self).__init__(**kwargs)
-        self.text = 'Start Game'
+        self.pos_hint = { 'top':0.5, 'center_x':0.5 }
+        self.size_hint = (0.4,0.15)
+        self.text = '[b][size=30]Start Game[/size][/b]'
+        self.markup = True
 
 class QuitButton(Button):
     def __init__(self,**kwargs):
         super(QuitButton,self).__init__(**kwargs)
-        self.text = 'Quit Game'
+        self.pos_hint = { 'top':0.3, 'center_x':0.5 }
+        self.size_hint = (0.4,0.15)
+        self.text = '[b][size=25]Quit Game[/size][/b]'
+        self.markup = True
